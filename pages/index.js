7af500bfad742fb4ba3a0a798cc467ab7d1d1292
elimagal84-238 <elimagal84@gmail.com>
2025-09-13 11:21:10 +0300

    Create index.js

---
+++ pages/index.js
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <div style={{ textAlign: "center", marginTop: "50px" }}>
+      <h1>ברוך הבא ל-CV Magic 🚀</h1>
+      <p>זהו הדף הראשון שלך ב-Next.js</p>
+    </div>
+  );
+}
